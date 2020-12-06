0762436a2833a8f2590442ae2702524eec0fcced
Austin Ellis <Austin@Janets-iMac.local>
2020-12-05 23:58:57 -0500

    Finished adding MVC files

---
+++ controllers/burgerController.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const router = express.Router();
+
+const burger = require("../models/burgers");
+
+
+router.get("/", function(req, res) {
+    burger.selectAll().then(function(data) {
+        const hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    console.log(req.body.burger_name).then(function(data) {
+        res.redirect("/");
+    });
+});
