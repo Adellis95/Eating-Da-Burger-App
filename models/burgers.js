bd6cbcf8776258b2283a94a11c0f91e7d79996be
Austin Ellis <Austin@Janets-iMac.local>
2020-12-07 17:49:08 -0500

    added my burger.js

--- models/burgers.js
+++ models/burgers.js
@@ -1,2 +1,16 @@
+const { updateOne } = require("../config/orm");
 const orm = require("../config/orm");
 
+let burger = {
+    selectAll: () => {
+        return orm.selectAll("burgers");
+    },
+    insertOne: burgerName => {
+        return orm.insertOne("burgers", "burger_name", burgerName);
+    },
+    updateOne: (updateObject, id) => {
+        return orm.updateOne("burgers", updateObject, id);
+    }
+};
+
+module.exports = burger;
